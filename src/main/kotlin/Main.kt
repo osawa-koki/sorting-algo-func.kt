bd4c7b763054d27e733fa85e3e1dcba0d9e26faf
osawa-koki <github@osawa.dev>
2023-02-22 22:01:16 +0900

    配列の表示。

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
-fun main(args: Array<String>) {
-  println("Hello World!")
 
-  // Try adding program arguments via Run/Debug configuration.
-  // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-  println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+
+  val ary = Array(10, { i -> i })
+
+  // 配列を表示
+  Display("Organized", ary)
+
 }
